db3b9793b442e426143b26c28630c639f856dcd5
Xvaa1 <68472357+Xvaa1@users.noreply.github.com>
2021-02-28 18:05:15 -0600

    Create TM.lua

---
+++ TM.lua
@@ -0,0 +1,21 @@
+--[[
+_______   _____                             
+\   _  \_/ ____\____  _____    ______ ______
+/  /_\  \   __\\__  \ \__  \  / ____//  ___/
+\  \_/   \  |   / __ \_/ __ \< <_|  |\___ \ 
+ \_____  /__|  (____  (____  /\__   /____  >
+       \/           \/     \/    |__|    \/ 
+       
+       Discord Creator: foreign#0005
+       
+       Roblox Name: 4I78O
+
+       Released on 2/28/2021 @ 6:05 PM / Last Updated on 2/28/2021 @ 6:05 PM
+]]
+
+-- Support Version (V2.1)
+if syn then
+    loadstring(syn.request({Url = 'https://raw.githubusercontent.com/Xvaa1/Folder/master/Support.lua', Method = "GET"}).Body)()
+elseif IsElectron then
+    loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/Xvaa1/Folder/master/Support.lua"), true))()
+end
